60920be9bbdf7fcde1667a879932ce3fd8a555ea
Eleanor Smith <esmith88@jasmin-sci5.ceda.ac.uk>
2020-02-20 13:45:57 +0000

    more tests for scan

--- test/test_scan.py
+++ test/test_scan.py
@@ -3,8 +3,8 @@ import subprocess
 import glob
 import sys
 import pytest
+import os
 
-import SETTINGS
 import scan
 
 
@@ -81,14 +81,33 @@ def test_output_to_JSON(tmpdir, create_netcdf_file):
     JSON = scan.output_to_JSON(characteristics, output_path, json_file_name, output_error_path, var_id)
 
     assert JSON == False
-#     
+     
+
+def test_already_run_output():
+    # check nothing returned if success file already produced
+    fpath = 'ALL_OUTPUTS/cmip5/output1/MOHC/HadGEM2-ES/historical/JSON_outputs/r1i1p1/cmip5.output1.MOHC.HadGEM2-ES.historical.mon.land.Lmon.r1i1p1.latest.rh.json'
+    if os.path.exists(fpath):
+        os.unlink(fpath)
+    cmd = 'python scan.py -m MOHC/HadGEM2-ES -exp historical -e r1i1p1 -v rh'
+    subprocess.call(cmd, shell=True)
+    scanner = scan.scan('MOHC/HadGEM2-ES', 'historical', 'r1i1p1', 'rh')
+    assert scanner is None
+
+    cmd_delete = 'rm -r ALL_OUTPUTS/cmip5/output1/MOHC/HadGEM2-ES/historical/JSON_outputs/r1i1p1/cmip5.output1.' \
+                 'MOHC.HadGEM2-ES.historical.mon.land.Lmon.r1i1p1.latest.rh.json ' \
+                 'ALL_OUTPUTS/cmip5/output1/MOHC/HadGEM2-ES/historical/success_files/r1i1p1/rh.log'
+    subprocess.call(cmd_delete, shell=True)
+
+
+def test_scan_no_files():
+    # no files for this file path
+    scanner = scan.scan('MOHC/HadGEM2-ES', 'historical', 'r1i1p1', 'cMisc')
+    assert scanner == False
+
 # def test_loop_over_vars():
-# 
-#     
-# # test scan - test false produced when supposed to and test already ran success file output
-# def test_already_run_output():
-#     pattern = '/badc/cmip5/data/cmip5/output1/{model}/historical/mon/land/Lmon' \
-#               '/{ensemble}/latest/{var_id}/*.nc'
-#     model = 'MOHC/HadGEM2-ES'
-#     ensemble = 'r1i1p1'
-#     var_id = 'rh'
+#
+# def test_scan_extract_error(): # need example of a file that has a characteristic that can't be extracted
+#
+# def test_scan_output_error(): # need example of a file that has a characteristic that can't be dumped to json file
+#
+# def test_scan_open_error(): # need example of a file set that can't be opened using mfdataset
