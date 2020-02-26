075c50c18e333ac99a2056a8a3a2a15e0096d1b6
agstephens <ag.stephens@stfc.ac.uk>
2020-02-26 15:25:03 +0000

    Added analyse.py

--- analyse.py
+++ analyse.py
@@ -1,6 +1,10 @@
 import argparse
+import collections
+import json
 
-from SETTINGS import _base_path
+import SETTINGS
+from lib import options
+
 
 
 def _get_arg_parser():
@@ -33,21 +37,77 @@ def parse_args():
     args = parser.parse_args()
 
     project = args.project[0]
-    ds_ids = args.dataset_ids.split(',')
+    ds_ids = args.dataset_ids[0].split(',')
 
     return project, ds_ids
 
 
-def analyse_datasets(ds_ids):
+def _lookup(item, *keys):
+
+    keys = list(keys)
+    
+    while keys:
+        k0 = keys[0]
+        item = item[k0]
+        keys.remove(k0)
+
+    return item
+        
+
+def analyse_characteristic(records, *keys):
+    
+    results = {}
+    count = 0
+
+    for ds_id, rec in records.items():
+        
+        result = _lookup(rec, *keys)
+        results.setdefault(result, [])
+        results[result].append(ds_id)
+        count += 1
+
+    print(f'\n[INFO] Testing: {keys} - found {len(results)} varieties')
+    for key in sorted(results):
+
+        ds_ids = results[key]
+        print(f'\t{keys} == {key}:   {len(ds_ids)}')
+        count_ratio = float(len(ds_ids)) / count
+
+        if count_ratio < SETTINGS.CONCERN_THRESHOLD:
+            print(f'\t[WARN] SUGGEST FIX OF {keys} ON:\n\t\t' + '\n\t\t'.join(ds_ids))
+    
+
+def analyse_datasets(project, ds_ids):
     "Compares a set of dataset identifiers"
+    records = load_records(ds_ids)
 
-   sdfds 
+    analyse_characteristic(records, 'data', 'rank')
+    analyse_characteristic(records, 'coordinates', 'time', 'calendar')
 
+
+def load_records(ds_ids):
+
+    gl = SETTINGS.DIR_GROUPING_LEVEL
+    records = collections.OrderedDict()
+
+    for ds_id in ds_ids:
+
+        parts = ds_id.split('.')
+        grouped_ds_id = '/'.join(parts[:-gl]) + '/' + '.'.join(parts[-gl:])
+
+        json_path = SETTINGS.JSON_OUTPUT_PATH.format(**vars())
+
+        with open(json_path) as reader:
+            records[ds_id] = json.load(reader)
+        
+
+    return records
+    
 
 def main():
     
     project, ds_ids = parse_args()
-    analyse_datasets(ds_ids)
+    analyse_datasets(project, ds_ids)
 
 
 if __name__ == '__main__':
